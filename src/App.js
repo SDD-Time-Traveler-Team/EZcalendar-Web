2104fbe6622c078f58601133d97ca010f8884aad
wodecinghaolen <yotsuba8629@outlook.com>
2022-10-02 18:10:28 -0400

    Added login page
    (still has bug)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
 import './App.css';
 import CalendarUI from './components/CalendarUI'
 import React from "react";
+import LoginPage from './components/LoginPage'
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
-class App extends React.Component {
-    render(){
+//class App extends React.Component {
+function App() {
+    // render(){
+        const ROUTES = {
+            MAINPAGE: "/",
+            LOGIN: "/login",
+        };
+
         return (
-            <>
-                <CalendarUI/>
-            </>
+            // <BrowserRouter>
+                <main>
+                    <Routes>
+                        {/* <Route
+                            exact path={"/"}
+                            element={<Navigate to={ROUTES.MAINPAGE}/>}
+                        />
+                        <Route
+                            exact path={ROUTES.LOGIN}
+                            element={<LoginPage/>}
+                        /> */}
+                        <Route
+                            exact path={ROUTES.MAINPAGE}
+                            element={<>Hello World</>}
+                        />
+                    </Routes>
+                </main>
+            // </BrowserRouter>
         );
-    }
+    // }
 }
+
+/*
+<BrowserRouter>
+    <Router>
+        <div>
+            <Route path="/" component={CalendarUI} />
+            <Route exact path="/login" component={LoginPage} />
+        </div>
+    </Router>
+</BrowserRouter>
+*/
 
 export default App;
