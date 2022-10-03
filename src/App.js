3881e406b4a807414899e5fa5c87e34c94b1a392
Ji Wang <wj42421999@gmail.com>
2022-10-03 01:23:57 -0400

    Fix infinite navigation from "/" to "/"

--- src/App.js
+++ src/App.js
@@ -2,48 +2,30 @@ import './App.css';
 import CalendarUI from './components/CalendarUI'
 import React from "react";
 import LoginPage from './components/LoginPage'
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-//class App extends React.Component {
-function App() {
-    // render(){
+class App extends React.Component {
+    render() {
         const ROUTES = {
-            MAINPAGE: "/",
             LOGIN: "/login",
+            CALENDAR: "/calendar"
         };
 
         return (
-            // <BrowserRouter>
-                <main>
-                    <Routes>
-                        {/* <Route
-                            exact path={"/"}
-                            element={<Navigate to={ROUTES.MAINPAGE}/>}
-                        />
-                        <Route
-                            exact path={ROUTES.LOGIN}
-                            element={<LoginPage/>}
-                        /> */}
-                        <Route
-                            exact path={ROUTES.MAINPAGE}
-                            element={<>Hello World</>}
-                        />
-                    </Routes>
-                </main>
-            // </BrowserRouter>
+            <BrowserRouter>
+                <Routes>
+                    <Route
+                        exact path={ROUTES.LOGIN}
+                        element={<LoginPage/>}
+                    />
+                    <Route
+                        exact path={ROUTES.CALENDAR}
+                        element={<CalendarUI/>}
+                    />
+                </Routes>
+            </BrowserRouter>
         );
-    // }
+    }
 }
-
-/*
-<BrowserRouter>
-    <Router>
-        <div>
-            <Route path="/" component={CalendarUI} />
-            <Route exact path="/login" component={LoginPage} />
-        </div>
-    </Router>
-</BrowserRouter>
-*/
 
 export default App;
