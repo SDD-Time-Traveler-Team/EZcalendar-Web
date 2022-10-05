a25f7d9bd88ff89b0a3d6a4a4bf9969eb0ff005f
Ji Wang <wj42421999@gmail.com>
2022-10-05 15:50:28 -0400

    Reformat login and signup pages

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,90 +3,90 @@ import React from 'react';
 
 class LoginPage extends React.Component {
 
-  render () {
-    const onLogin = (values) => {
-      console.log('Success:', values);
-    };
-  
-    const onLoginFailed = (errorInfo) => {
-      console.log('Failed:', errorInfo);
-    };
+    render() {
+        const onLogin = (values) => {
+            console.log('Success:', values);
+        };
 
-    return (
-      <Row type="flex" justify="center" align="middle" style={{minHeight: '75vh'}}>
-        <Form
-          name="basic"
-          labelCol={{
-            span: 10,
-          }}
-          wrapperCol={{
-            span: 4,
-          }}
-          initialValues={{
-            remember: true,
-          }}
-          style={{
-            width: '100%',
-          }}
-          onFinish={onLogin}
-          onFinishFailed={onLoginFailed}
-          autoComplete="off"
-        >
-          <Form.Item required={false}
-            name="email"
-            label="E-mail"
-            rules={[
-              {
-                type: 'email',
-                message: 'The input is not valid E-mail!',
-              },
-              {
-                required: true,
-                message: 'Please input your E-mail!',
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-    
-          <Form.Item required={false}
-            label="Password"
-            name="password"
-            rules={[
-              {
-                required: true,
-                message: 'Please input your password!',
-              },
-            ]}
-          >
-            <Input.Password />
-          </Form.Item>
-    
-          <Form.Item
-            name="remember"
-            valuePropName="checked"
-            wrapperCol={{
-              offset: 10,
-              span: 16,
-            }}
-          >
-            <Checkbox>Remember me</Checkbox>
-          </Form.Item>
-    
-          <Form.Item
-            wrapperCol={{
-              offset: 10,
-              span: 16,
-            }}
-          >
-            <Button type="primary" htmlType="submit">
-              Sign in
-            </Button> Or <a href="/signup">register now!</a>
-          </Form.Item>
-        </Form>
-      </Row>
-    );
-  }
+        const onLoginFailed = (errorInfo) => {
+            console.log('Failed:', errorInfo);
+        };
+
+        return (
+            <Row type="flex" justify="center" align="middle" style={{minHeight: '75vh'}}>
+                <Form
+                    name="basic"
+                    labelCol={{
+                        span: 10,
+                    }}
+                    wrapperCol={{
+                        span: 4,
+                    }}
+                    initialValues={{
+                        remember: true,
+                    }}
+                    style={{
+                        width: '100%',
+                    }}
+                    onFinish={onLogin}
+                    onFinishFailed={onLoginFailed}
+                    autoComplete="off"
+                >
+                    <Form.Item required={false}
+                               name="email"
+                               label="E-mail"
+                               rules={[
+                                   {
+                                       type: 'email',
+                                       message: 'The input is not valid E-mail!',
+                                   },
+                                   {
+                                       required: true,
+                                       message: 'Please input your E-mail!',
+                                   },
+                               ]}
+                    >
+                        <Input/>
+                    </Form.Item>
+
+                    <Form.Item required={false}
+                               label="Password"
+                               name="password"
+                               rules={[
+                                   {
+                                       required: true,
+                                       message: 'Please input your password!',
+                                   },
+                               ]}
+                    >
+                        <Input.Password/>
+                    </Form.Item>
+
+                    <Form.Item
+                        name="remember"
+                        valuePropName="checked"
+                        wrapperCol={{
+                            offset: 10,
+                            span: 16,
+                        }}
+                    >
+                        <Checkbox>Remember me</Checkbox>
+                    </Form.Item>
+
+                    <Form.Item
+                        wrapperCol={{
+                            offset: 10,
+                            span: 16,
+                        }}
+                    >
+                        <Button type="primary" htmlType="submit">
+                            Sign in
+                        </Button> Or <a href="/signup">register now!</a>
+                    </Form.Item>
+                </Form>
+            </Row>
+        );
+    }
 };
 
 export default LoginPage;
--- src/components/SignupPage.js
+++ src/components/SignupPage.js
@@ -4,149 +4,150 @@ import {
     Form,
     Input,
     Row,
-  } from 'antd';
-  import React from "react";
-  const formItemLayout = {
+} from 'antd';
+import React from "react";
+
+const formItemLayout = {
     labelCol: {
-      xs: {
-        span: 4,
-      },
-      sm: {
-        span: 10,
-      },
+        xs: {
+            span: 4,
+        },
+        sm: {
+            span: 10,
+        },
     },
     wrapperCol: {
-      xs: {
-        span: 4,
-      },
-      sm: {
-        span:4,
-      },
+        xs: {
+            span: 4,
+        },
+        sm: {
+            span: 4,
+        },
     },
-  };
-  const tailFormItemLayout = {
+};
+const tailFormItemLayout = {
     wrapperCol: {
-      xs: {
-        span: 4,
-        offset: 0,
-      },
-      sm: {
-        span: 10,
-        offset: 10,
-      },
+        xs: {
+            span: 4,
+            offset: 0,
+        },
+        sm: {
+            span: 10,
+            offset: 10,
+        },
     },
-  };
-  
-  const SignupPage = () => {
+};
+
+const SignupPage = () => {
 
     const onSignup = (values) => {
-      console.log('Received values of form: ', values);
+        console.log('Received values of form: ', values);
     };
 
     return (
-      <Row type="flex" justify="center" align="middle" style={{minHeight: '75vh'}}>
-        <Form
-          {...formItemLayout}
-          name="register"
-          onFinish={onSignup}
-          scrollToFirstError
-          style={{
-            width: '100%',
-          }}
-        >
-          <Form.Item
-            name="email"
-            label="E-mail"
-            rules={[
-              {
-                type: 'email',
-                message: 'The input is not valid E-mail!',
-              },
-              {
-                required: true,
-                message: 'Please input your E-mail!',
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-    
-          <Form.Item
-            name="password"
-            label="Password"
-            rules={[
-              {
-                required: true,
-                message: 'Please input your password!',
-              },
-            ]}
-            hasFeedback
-          >
-            <Input.Password />
-          </Form.Item>
-    
-          <Form.Item
-            name="confirm"
-            label="Confirm Password"
-            dependencies={['password']}
-            hasFeedback
-            rules={[
-              {
-                required: true,
-                message: 'Please confirm your password!',
-              },
-              ({ getFieldValue }) => ({
-                validator(_, value) {
-                  if (!value || getFieldValue('password') === value) {
-                    return Promise.resolve();
-                  }
-    
-                  return Promise.reject(new Error('The two passwords that you entered do not match!'));
-                },
-              }),
-            ]}
-          >
-            <Input.Password />
-          </Form.Item>
-    
-          <Form.Item
-            name="username"
-            label="Username"
-            tooltip="What do you want others to call you?"
-            rules={[
-              {
-                required: true,
-                message: 'Please input your username!',
-                whitespace: true,
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-    
-          <Form.Item
-            name="agreement"
-            valuePropName="checked"
-            rules={[
-              {
-                validator: (_, value) =>
-                  value ? Promise.resolve() : Promise.reject(new Error('Should accept agreement')),
-              },
-            ]}
-            {...tailFormItemLayout}
-          >
-            <Checkbox>
-              I have read the <a href="">agreement</a>
-            </Checkbox>
-          </Form.Item>
-          <Form.Item {...tailFormItemLayout}>
-            <Button type="primary" htmlType="submit">
-              Register
-            </Button> Or <a href="/login">Go sign in.</a>
-          </Form.Item>
-        </Form>
-      </Row>
+        <Row type="flex" justify="center" align="middle" style={{minHeight: '75vh'}}>
+            <Form
+                {...formItemLayout}
+                name="register"
+                onFinish={onSignup}
+                scrollToFirstError
+                style={{
+                    width: '100%',
+                }}
+            >
+                <Form.Item
+                    name="email"
+                    label="E-mail"
+                    rules={[
+                        {
+                            type: 'email',
+                            message: 'The input is not valid E-mail!',
+                        },
+                        {
+                            required: true,
+                            message: 'Please input your E-mail!',
+                        },
+                    ]}
+                >
+                    <Input/>
+                </Form.Item>
+
+                <Form.Item
+                    name="password"
+                    label="Password"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your password!',
+                        },
+                    ]}
+                    hasFeedback
+                >
+                    <Input.Password/>
+                </Form.Item>
+
+                <Form.Item
+                    name="confirm"
+                    label="Confirm Password"
+                    dependencies={['password']}
+                    hasFeedback
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please confirm your password!',
+                        },
+                        ({getFieldValue}) => ({
+                            validator(_, value) {
+                                if (!value || getFieldValue('password') === value) {
+                                    return Promise.resolve();
+                                }
+
+                                return Promise.reject(new Error('The two passwords that you entered do not match!'));
+                            },
+                        }),
+                    ]}
+                >
+                    <Input.Password/>
+                </Form.Item>
+
+                <Form.Item
+                    name="username"
+                    label="Username"
+                    tooltip="What do you want others to call you?"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your username!',
+                            whitespace: true,
+                        },
+                    ]}
+                >
+                    <Input/>
+                </Form.Item>
+
+                <Form.Item
+                    name="agreement"
+                    valuePropName="checked"
+                    rules={[
+                        {
+                            validator: (_, value) =>
+                                value ? Promise.resolve() : Promise.reject(new Error('Should accept agreement')),
+                        },
+                    ]}
+                    {...tailFormItemLayout}
+                >
+                    <Checkbox>
+                        I have read the <a href="">agreement</a>
+                    </Checkbox>
+                </Form.Item>
+                <Form.Item {...tailFormItemLayout}>
+                    <Button type="primary" htmlType="submit">
+                        Register
+                    </Button> Or <a href="/login">Go sign in.</a>
+                </Form.Item>
+            </Form>
+        </Row>
     );
-  };
-  
-  export default SignupPage;
+};
+
+export default SignupPage;
