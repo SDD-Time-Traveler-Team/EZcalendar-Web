285b78181c7e2f95d9406a46063f58ca41be7bd6
Fiazori <57635584+Fiazori@users.noreply.github.com>
2022-10-20 16:30:34 -0400

    change layout to taglist(left) calendar(right)
    added row and col in calendarUI to change the layout

--- src/components/CalendarUI.js
+++ src/components/CalendarUI.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import CalendarViewUI from "./CalendarViewUI";
+import TagView from "./TagView";
+import { Row, Col } from 'antd';
 
 class CalendarUI extends React.Component {
 
         render() {
             return(
             <>
-                <CalendarViewUI/>
+                <Row>
+                    <Col span = {6}>
+                        <TagView/>
+                    </Col>
+                    <Col span = {18}>
+                        <CalendarViewUI/>
+                    </Col>
+                </Row>
             </>
 
             )
