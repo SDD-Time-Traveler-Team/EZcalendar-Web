5e485f2bf20422677a8c230425b1ef653e2cb10c
Fiazori <57635584+Fiazori@users.noreply.github.com>
2022-10-24 15:28:28 -0400

    UI changes for tag cards
    added edit and delete button, change layout for title duration and description

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -1,10 +1,21 @@
 import React, { useState } from 'react';
+import { EditOutlined, DeleteOutlined} from '@ant-design/icons';
 import {Card} from 'antd';
+import Meta from "antd/es/card/Meta";
 const Tag = (props) => {
     return (
         <>
-            <Card class='color' title={props.title} style={{ width:'90%', marginTop: 5, background: "yellow" }}>
-                <p>{props.duration}</p>
+            <Card style={{marginLeft:"2.5%", width:'95%', marginTop: 5}}
+                  title={props.title}
+                  extra={props.duration}
+                  hoverable={true}
+                  actions={[
+                      <EditOutlined key="edit"/>,
+                      <DeleteOutlined key="delete"/>
+                  ]}>
+                <Meta
+                    description={props.description}
+                />
             </Card>
         </>
     );
