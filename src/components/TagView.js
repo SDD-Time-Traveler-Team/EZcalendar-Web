59f8c6bf462c2501209de3ff93b1469943d96d1e
Fiazori <57635584+Fiazori@users.noreply.github.com>
2022-10-24 14:42:58 -0400

    implemented tag view function and data structure
    added tags state and fake initialstate for testing. Tags can be viewed in taglist now.

--- src/components/TagView.js
+++ src/components/TagView.js
@@ -1,23 +1,33 @@
 //import { Card, Space, Button } from 'antd';
-import React from 'react';
+import React, {useState} from 'react';
 import { Row, Col } from 'antd';
-import TagCreate from './TagCreate'
+import TagCreate from './TagCreate';
+import Tag from './Tag';
 
-const TagView = () => (
-    <Row>
-        <Col
-            xs={{
-                span: 5,
-                offset: 1,
-            }}
-            lg={{
-                span: 6,
-                offset: 2,
-            }}
-        >
-            <TagCreate/>
-        </Col>
-    </Row>
-);
+
+const TagView = () => {
+    const [tags, setTags] = useState([
+        {title:"event1", duration: "3h"},
+        {title:"event2", duration: "2h"},
+        {title:"event3", duration: "5h"},
+    ]);
+
+
+    return (
+        <Row>
+            <Col>
+                <Row gutter={23}>
+                    <TagCreate/>
+                </Row>
+                <Row gutter={23}>
+                    {tags.map(item=>(
+                        <Tag key = {item.title} title={item.title} duration={item.duration}/>
+                    ))}
+                </Row>
+
+            </Col>
+        </Row>
+    )
+}
 
 export default TagView;
