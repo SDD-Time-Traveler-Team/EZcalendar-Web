96e7d4f9023fc24cd028d2c44da96e414c03bb1c
Fiazori <57635584+Fiazori@users.noreply.github.com>
2022-10-25 13:50:44 -0400

    Update TagCreate.js
    delete unnecessary code and added input validation check

--- src/components/TagCreate.js
+++ src/components/TagCreate.js
@@ -5,40 +5,22 @@ import {
     Input,
     Radio,
     Form,
-    // Cascader,
-    // InputNumber,
-    // Select,
-    // Switch,
-    // TreeSelect,
-    // DatePicker,  
-    // TimePicker,
-} from 'antd';
+    TimePicker,
+    Cascader,
+    InputNumber,
+    Select,
+    Switch,
+    TreeSelect,
+    DatePicker,
 
-const formItemLayout = {
-    // labelCol: {
-    //     xs: {
-    //         span: 24,
-    //     },
-    //     sm: {
-    //         span: 8,
-    //     },
-    // },
-    // wrapperCol: {
-    //     xs: {
-    //         span: 24,
-    //     },
-    //     sm: {
-    //         span: 16,
-    //     },
-    // },
-};
+} from 'antd';
 
 const config = {
     rules: [
         {
             type: 'object',
             required: true,
-            message: 'Please select time!',
+            message: 'Please select time',
         },
     ],
 };
@@ -46,6 +28,8 @@ const config = {
 const TagCreate = () => {
 
     const [value, setValue] = useState(1);
+    var inputtitle = "";
+    var inputduration = ""
 
     const onChange = (e) => {
         console.log('radio checked', e.target.value);
@@ -80,30 +64,38 @@ const TagCreate = () => {
             rangeTimeValue[0].format('YYYY-MM-DD HH:mm:ss'),
             rangeTimeValue[1].format('YYYY-MM-DD HH:mm:ss'),
             ],
-            'time-picker': fieldsValue['time-picker'].format('HH:mm:ss'),
+            'time-picker': fieldsValue['time-picker'].format('HH:mm'),
         };
         console.log('Received values of form: ', values);
     };
-
+    const [form] = Form.useForm();
     return (
         <>
             <Button type="primary" onClick={showModal}>
                 Create Tag
             </Button>
-            <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-                <Form name="time_related_controls" {...formItemLayout} onFinish={onFinish}>
-                    <Form.Item {...config}>
-                        <Radio.Group onChange={onChange} value={value}>
-                            <Radio value={1}>
-                                Event
-                            </Radio>
-                            <Radio value={2}>
-                                To-do
-                            </Radio>
-                        </Radio.Group>
-                    </Form.Item>
+            <Modal title="Create a tag" open={isModalOpen} onOk={() => {
+                form
+                .validateFields().then(handleOk)}} onCancel={handleCancel}>
+                <Form form={form} name="time_related_controls" onFinish={onFinish}>
+                    {/*<Form.Item>*/}
+                    {/*    <Radio.Group onChange={onChange} value={value}>*/}
+                    {/*        <Radio value={1}>*/}
+                    {/*            Event*/}
+                    {/*        </Radio>*/}
+                    {/*        <Radio value={2}>*/}
+                    {/*            To-do*/}
+                    {/*        </Radio>*/}
+                    {/*    </Radio.Group>*/}
+                    {/*</Form.Item>*/}
+
                     <Form.Item 
                         label="Title"
+                        name="title"
+                        rules={[{
+                            required: true,
+                            message: "Please enter a title"
+                        }]}
                         wrapperCol={{
                             xs: {
                                 span: 0,
@@ -118,20 +110,24 @@ const TagCreate = () => {
                         <Input/>
                     </Form.Item>
 
-                    {/* <Form.Item name="date-time-picker" label="Date" {...config} required={false}>
-                        <DatePicker showTime format="YYYY-MM-DD" />
+                    <Form.Item name="time-picker" label="Duration" {...config}>
+                        <TimePicker format="HH:mm"/>
                     </Form.Item>
-                    <Form.Item name="time-picker" label="TimePicker" {...config} required={false}>
-                    <DatePicker.RangePicker showTime format="YYYY-MM-DD HH:mm:ss" />
-                    </Form.Item>
-                    <Form.Item label="Repeat" name="size">
-                        <Radio.Group>
-                            <Radio.Button value="1">once a day</Radio.Button>
-                            <Radio.Button value="2">once a week</Radio.Button>
-                            <Radio.Button value="3">once a month</Radio.Button>
-                            <Radio.Button value="4">none</Radio.Button>
-                        </Radio.Group>
-                    </Form.Item> */}
+                    {/*<Form.Item*/}
+                    {/*    label="Description"*/}
+                    {/*    wrapperCol={{*/}
+                    {/*        xs: {*/}
+                    {/*            span: 0,*/}
+                    {/*            offset: 0,*/}
+                    {/*        },*/}
+                    {/*        sm: {*/}
+                    {/*            span: 15,*/}
+                    {/*            offset: 0,*/}
+                    {/*        },*/}
+                    {/*    }}*/}
+                    {/*>*/}
+                    {/*    <Input.TextArea/>*/}
+                    {/*</Form.Item>*/}
                 </Form>
             </Modal>
         </>
