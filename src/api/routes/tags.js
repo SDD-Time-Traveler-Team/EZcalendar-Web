4936a178fc1efb6fd5dc75e37ceaa84661153bce
katyushasama <aoyamamegumi811@gmail.com>
2022-10-26 00:40:38 -0400

    init tags function for backend

---
+++ src/api/routes/tags.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const router = express.Router()
+const { getTags, createTag, updateTag, deleteTag } = require('../utils/database.js')
+
+// GET /tags read all tags 
+router.get("/", (req, res, next) => {
+    getTags(req.body.userId).then((resolve, reject) => {
+        if (resolve) {
+            res.sendStatus(200)
+        } else {
+            res.send(400)
+        }
+    });
+});
+
+// Put /tags create a new tag
+router.put("/", (req, res, next) => {
+    createTag(req.body.userId, req.body.tag).then((resolve, reject) => {
+        if (resolve) {
+            res.sendStatus(200)
+        } else {
+            res.sendStatus(400)
+        }
+    })
+})
+
+// POST /tags update with specific id 
+router.post("/", (req, res, next) => {
+    updateTag(req.body.userId, req.body.tagId).then((resolve, reject) => {
+        if (resolve) {
+            res.sendStatus(200)
+        } else {
+            res.send(400)
+        }
+    })
+})
+
+// DELETE /tags delete with specific id
+router.delete("/", (req, res, next) => {
+    deleteTag(req.body.userId, req.body.tagId).then((resolve, reject) => {
+        if (resolve) {
+            res.sendStatus(200)
+        } else {
+            res.send(400)
+        }
+    })
+})
--- src/api/utils/database.js
+++ src/api/utils/database.js
@@ -10,4 +10,52 @@ const client = new Client({
     port: process.env.DBPORT,
 })
 
+const getTags = async (userId) => {
+    client.query(
+        `SELECT
+            id, email, title 
+        FROM 
+            tags 
+        WHERE 
+            email = ${userId};`, (error, result) => {
+        if (error){
+            console.log("Error with get_tags", error.stack);
+        }
+    })
+}
+
+const createTag = async (userId, tag) => {
+    client.query(
+        `INSERT INTO tags (title, email) 
+        VALUES (${tags.title}, ${tags.email});`, (error, result) => {
+        if (error) {
+            console.log("Error with create_tags", error.stack);
+        }
+    })
+}
+
+
+const updateTag = async (userId, tag) => {
+    client.query(`UPDATE tags SET title=${tag.title} WHERE ${userId}=email AND ${tag.id}=id;`, (err, res) => {
+        if (err) {
+            console.log(err.stack)
+        }
+    })
+}
+
+const deleteTag = async (userId, tagId) => {
+    client.query(`DELETE FROM tags WHERE email=${userId} AND id=${tagId};`, (err, res) => {
+        if (err) {
+            console.log(err.stack)
+        }
+    })
+}
+
 client.connect().then(() => console.log("client connected"))
+
+module.exports = {
+    getTags,
+    createTag,
+    updateTag,
+    deleteTag
+}
