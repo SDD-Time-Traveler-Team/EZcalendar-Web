685bd55611104b5aeec87be5c368a2ccdc8acd3b
Ji Wang <wj42421999@gmail.com>
2022-10-30 22:47:16 -0400

    Refactor /tags APIs

--- src/api/routes/tags.js
+++ src/api/routes/tags.js
@@ -1,49 +1,48 @@
-const express = require('express')
-const router = express.Router()
-const { getTags, createTag, updateTag, deleteTag } = require('../utils/database.js')
+const express = require("express");
+const router = express.Router();
+const {getAllTags, createTag, updateTag, deleteTag} = require("../utils/database.js");
 
-// GET /tags read all tags 
-router.get("/", (req, res, next) => {
-    getTags(req.body.userId).then((resolve, reject) => {
-        if (resolve) {
-            res.sendStatus(200)
-        } else {
-            res.send(400)
-        }
+// GET /tags read all tags
+router.get("/", (req, res) => {
+    getAllTags(req.body.email).then((result) => {
+        res.send(result.rows[0]);
+    }).catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
     });
 });
 
 // Put /tags create a new tag
-router.put("/", (req, res, next) => {
-    createTag(req.body.userId, req.body.tag).then((resolve, reject) => {
-        if (resolve) {
-            res.sendStatus(200)
-            res.end(JSON.stringify({id:resolve.rows[0].id}))
-        } else {
-            res.sendStatus(400)
-        }
-    })
-})
+router.put("/", (req, res) => {
+    const {email, tagTitle, durationInMinutes} = req.body;
+    createTag(email, tagTitle, durationInMinutes).then((result) => {
+        res.send(result.rows[0]);
+    }).catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+    });
+});
 
-// POST /tags update with specific id 
-router.post("/", (req, res, next) => {
-    updateTag(req.body.userId, req.body.tagId).then((resolve, reject) => {
-        if (resolve) {
-            res.sendStatus(200)
-        } else {
-            res.send(400)
-        }
-    })
-})
+// POST /tags update with specific id
+router.post("/", (req, res) => {
+    const {email, tagId, tagTitle, durationInMinutes} = req.body;
+    updateTag(email, tagId, tagTitle, durationInMinutes).then((result) => {
+        res.send(result.rows[0]);
+    }).catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+    });
+});
 
 // DELETE /tags delete with specific id
-router.delete("/", (req, res, next) => {
-    deleteTag(req.body.userId, req.body.tagId).then((resolve, reject) => {
-        if (resolve) {
-            res.sendStatus(200)
-        } else {
-            res.send(400)
-        }
-    })
-})
-module.exports=router;
+router.delete("/", (req, res) => {
+    const {email, tagId} = req.body;
+    deleteTag(email, tagId).then((result) => {
+        res.send(result.row[0]);
+    }).catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+    });
+});
+
+module.exports = router;
